2a0866185ab01a0220caae907bf69131309c0912
yunfei <snowleopard331@163.com>
2016-01-12 21:45:14 -0800

    add boost thread asio test note

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,3 +1,13 @@
+/*
+    基于Boost.Asio来提高程序的可扩展性推荐的方法是:
+        采用单个I/O service多次调用run()方法
+
+    也有另外的方法可以选择:
+        可以创建多个I/O service 而不是将所有的线程都绑定到一个I/O service上
+        每个 I/O service 对应于一个线程 
+        如果 I/O service 的个数和计算机的核数相匹配 异步操作将会在各自对应的核上运行
+*/
+
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
