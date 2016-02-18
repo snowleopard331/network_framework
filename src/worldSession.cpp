4362f44b27ddd89a09d1467e07bf13abea00182e
yunfei <snowleopard331@163.com>
2016-02-18 11:15:01 +0800

    modify bug

---
+++ src/worldSession.cpp
@@ -0,0 +1,12 @@
+/**
+* author    :   yunfei
+* email     :   snowleopard331@163.com
+* date      :   2016/02/18
+*/
+
+#include "worldSession.h"
+
+void WorldSession::Handle_NULL(WorldPacket& recvPacket)
+{
+
+}
