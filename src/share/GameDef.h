55d070401286cf8f955544d1922fccac2e62e2ec
yunfei <snowleopard331@163.com>
2016-02-19 16:03:57 +0800

    try to compile

--- src/share/GameDef.h
+++ src/share/GameDef.h
@@ -7,9 +7,6 @@
 #ifndef _GAME_DEF_H_
 #define _GAME_DEF_H_
 
-enum DebugInfo
-{
-    DEBUG_TEST_MSG,
-};
+
 
 #endif//_GAME_DEF_H_
--- src/share/PosixDaemon.cpp
+++ src/share/PosixDaemon.cpp
@@ -4,16 +4,65 @@
 * date      :   2016/01/30
 */
 
-#ifndef _POSIX_DAEMON_H_
-#define _POSIX_DAEMON_H_
+#include "PosixDaemon.h"
 
-#include "Common.h"
-#include "log/glogHelper.h"
+//pid_t parent_pid, sid = 0;
 
-void startDaemon(uint32 timeout = 10);
+//void daemonSignal(int s)
+//{
+//    if(getpid() != parent_pid)
+//    {
+//        return;
+//    }
+//
+//    if(s == SIGUSR1)
+//    {
+//        exit(EXIT_SUCCESS);
+//    }
+//
+//    if(sid)
+//    {
+//        kill(sid, s);
+//    }
+//
+//    exit(EXIT_FAILURE);
+//}
 
-void stopDaemon();
+void startDaemon(uint32 timeout /* = 10 */)
+{
+    // parent_pid = getpid();
+    pid_t pid;
 
-void detachDaemon();
+    //signal(SIGUSR1, daemonSignal);
+    //signal(SIGINT, daemonSignal);
+    //signal(SIGTERM, daemonSignal);
+    //signal(SIGALRM, daemonSignal);
 
-#endif//_POSIX_DAEMON_H_
+    pid = fork();
+
+    if(pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if(pid > 0)
+    {
+        //alarm(timeout);
+        //pause();
+        exit(EXIT_FAILURE);
+    }
+
+    umask(0);
+
+    pid_t sid = setsid();
+
+    if(sid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if((chdir("/")) < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+}
