acabc4063e441e2bc306300b78f23b79fe5d0085
yunfei <snowleopard331@163.com>
2016-02-19 14:16:41 +0800

    add test code

---
+++ src/share/GameDef.h
@@ -0,0 +1,15 @@
+/**
+* author    :   yunfei
+* email     :   snowleopard331@163.com
+* date      :   2016/02/19
+*/
+
+#ifndef _GAME_DEF_H_
+#define _GAME_DEF_H_
+
+enum DebugInfo
+{
+    DEBUG_TEST_MSG,
+};
+
+#endif//_GAME_DEF_H_
