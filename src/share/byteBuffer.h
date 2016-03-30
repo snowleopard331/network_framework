e26d2cf20d15925012c97fd79df495a275b4508e
yunfei <snowleopard331@163.com>
2016-03-30 16:45:07 +0800

    note the endian code, because cmake is not ok

--- src/share/byteBuffer.h
+++ src/share/byteBuffer.h
@@ -8,7 +8,7 @@
 #define _BYTE_BUFFER_H_
 
 #include "Common.h"
-#include "byteConverter.h"
+//#include "byteConverter.h"
 
 class ByteBufferException
 {
@@ -83,7 +83,7 @@ public:
     template<typename T>
     void put(size_t pos, T value)
     {
-        EndianConvert(value);
+        // EndianConvert(value);
         put(pos, (uint8*)&value, sizeof(value));
     }
 
@@ -342,7 +342,7 @@ public:
             throw ByteBufferException(false, pos, sizeof(T), _storage.size());
         }
         T val = *((T const*)&_storage[pos]);
-        EndianConvert(val);
+        // EndianConvert(val);
         return val;
     }
 
@@ -409,7 +409,7 @@ private:
     template<typename T>
     void append(T value)
     {
-        EndianConvert(value);
+        // EndianConvert(value);
         append((uint8*)&value, sizeof(value));
     }
 
