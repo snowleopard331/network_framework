4ce11251a10dbde527699930d84da765ca2d3eb0
yunfei <snowleopard331@163.com>
2016-04-05 17:24:59 +0800

    add demo test stack

--- demo/testStack/main.cpp
+++ demo/testStack/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "StackTrace.h"
 
-int main()
+void fun1()
 {
     StackTrace stack;
     std::cout<<stack.c_str()<<std::endl;
+}
+
+void fun2()
+{
+    fun1();
+}
+
+int main()
+{
+    fun2()
 
     return 0;
 }
